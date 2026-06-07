e5868bc8038cf82228ae7ec81674b6de35c9ac11
Ícaro Rodrigues <desenvolvimento.web2@teiu.com.br>
2026-06-07 15:44:37 -0300

    fix: correção de rotas

--- api/routes/api.php
+++ api/routes/api.php
@@ -6,11 +6,10 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\ProductsController;
 
 Route::get('/products/{language}', [ProductsController::class, 'index']);
-Route::get('/products/{id}', [ProductsController::class, 'getProductbyId']);
+Route::get('/productsDetail/{id}', [ProductsController::class, 'getProductbyId']);
 
 Route::get('/test', [ContactController::class, 'test']);
 Route::post('/sendEmail', [ContactController::class, 'sendEmail']);
 Route::get('/pageWebsite/{path}', [PageController::class, 'index']);
 Route::get('/productsEmphasis', [ProductsController::class, 'getProductsWithEmphasisVariants']);
 
-Route::get('/products/{id}', [ProductsController::class, 'show']);
