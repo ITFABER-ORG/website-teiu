848b3a13ab5705bc0fc86db7ea0514d54f6cc77c
Ícaro Rodrigues <desenvolvimento.web2@teiu.com.br>
2026-07-06 10:47:40 -0300

    chore: configuração de portas

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
   ],
-})
+  server: {
+    host: "0.0.0.0", // opcional
+    port: 5174,
+    strictPort: true,
+  },
+});
